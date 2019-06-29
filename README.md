085facf59975b5150f698da3a6dd2294e2619460
Sajan Kuttipuravan <skpsajan@gmail.com>
2019-06-29 20:21:00 +0530

    Add Code upto Styling cmoponents

---
+++ README.md
@@ -0,0 +1,9 @@
+# The Road to Learn React - Hacker News Client
+
+[![Build Status](https://travis-ci.org/the-road-to-learn-react/hackernews-client.svg?branch=master)](https://travis-ci.org/the-road-to-learn-react/hackernews-client) [![Slack](https://slack-the-road-to-learn-react.wieruch.com/badge.svg)](https://slack-the-road-to-learn-react.wieruch.com/) [![Greenkeeper badge](https://badges.greenkeeper.io/the-road-to-learn-react/hackernews-client.svg)](https://greenkeeper.io/)
+
+The application you will build when you read the book to [learn React](https://www.robinwieruch.de/the-road-to-learn-react/).
+
+Find all chapter outcomes over [here](https://github.com/the-road-to-learn-react/hackernews-client).
+
+After reading the book, you can continue to [learn more about JavaScript](https://roadtoreact.com).
---
+++ src/App.js
@@ -0,0 +1,115 @@
+import React, { Component } from 'react';
+import './App.css';
+
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
+const isSearched = searchTerm => item =>
+  item.title.toLowerCase().includes(searchTerm.toLowerCase());
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      list,
+      searchTerm: '',
+    };
+
+    this.onSearchChange = this.onSearchChange.bind(this);
+    this.onDismiss = this.onDismiss.bind(this);
+  }
+
+  onSearchChange(event) {
+    this.setState({ searchTerm: event.target.value });
+  }
+
+  onDismiss(id) {
+    const isNotId = item => item.objectID !== id;
+    const updatedList = this.state.list.filter(isNotId);
+    this.setState({ list: updatedList });
+  }
+
+  render() {
+    const { searchTerm, list } = this.state;
+    return (
+      <div className="App">
+        <Search
+          value={searchTerm}
+          onChange={this.onSearchChange}
+        >
+          Search
+        </Search>
+        <Table
+          list={list}
+          pattern={searchTerm}
+          onDismiss={this.onDismiss}
+        />
+      </div>
+    );
+  }
+}
+
+const Search = ({value, onChange, children}) =>
+      <form>
+        { children }<input
+          type="text"
+          value={value}
+          onChange={onChange}
+        />
+      </form>
+
+const Table = ({list, pattern, onDismiss}) => {
+  return (
+      <div>
+        {list.filter(isSearched(pattern)).map(item =>
+          <div key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+            <span>
+              <Button
+                onClick={() => onDismiss(item.objectID)}
+                >
+                Dismiss
+              </Button>
+            </span>
+          </div>
+        )}
+      </div>
+    );
+  }
+
+const Button = ({className = '', onClick, children}) => {
+    return(
+      <button
+        onClick={onClick}
+        className={className}
+        type="button"
+      >
+        {children}
+      </button>
+    );
+  }
+
+
+export default App;
