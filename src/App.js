4bb4e4f8bfd1c55f94069b0834e95a603275d385
Sajan Kuttipuravan <skpsajan@gmail.com>
2019-07-03 12:12:45 +0530

    Add 'more' and 'results per page' option

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React, { Component } from 'react';
 import './App.css';
 
 const DEFAULT_QUERY = 'redux';
+const DEFAULT_HPP = '100';
 
 const PATH_BASE = 'https://hn.algolia.com/api/v1';
 const PATH_SEARCH = '/search';
 const PARAM_SEARCH = 'query=';
+const PARAM_PAGE = 'page=';
+const PARAM_HPP = 'hitsPerPage=';
 
-const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}`;
+const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}&${PARAM_PAGE}&${PARAM_HPP}`;
 
 console.log(url);
 
@@ -21,15 +24,23 @@ class App extends Component {
     };
   }
 
-  fetchSearchTopStories = (searchTerm) => {
-    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+  fetchSearchTopStories = (searchTerm, page = 0) => {
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
       .then(response => response.json())
       .then(result => this.setSearchTopStories(result))
       .catch(error => error);
   }
 
   setSearchTopStories = result => {
-    this.setState({ result });
+    const { hits, page } = result;
+    const oldHits = page === 0 ?
+                    [] :
+                    this.state.result.hits
+    const updatedHits = [...oldHits,...hits]
+
+    this.setState({
+      result: {hits: updatedHits}
+    })
   }
 
   componentDidMount() {
@@ -53,6 +64,7 @@ class App extends Component {
 
   render() {
     const { searchTerm, result } = this.state;
+    const page = (result && result.page) || 0
 
     return (
       <div className="page">
@@ -73,6 +85,11 @@ class App extends Component {
           />
           : "No results yet"
         }
+        <div className="interactions">
+          <Button onClick={() => this.fetchSearchTopStories(searchTerm, page + 1)}>
+            More
+          </Button>
+        </div>
       </div>
     );
   }
